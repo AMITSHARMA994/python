88becd0c33e8fdf19c0263c845b9b420cb8b58cb
AMITSHARMA994 <sharmaamit94420@gmail.com>
2023-01-31 21:06:00 -0500

    done

---
+++ new.py
@@ -0,0 +1,28 @@
+# a=tuple()
+# print(a)
+# b=(2,3,4,5)
+# print(b)
+# print(b[2:])
+
+#a='amit'
+#print(a.capitalize())
+#print(a.upper())
+#print(a.count('a'))
+#print(a.encode())
+#print(a.expandtabs())
+#print(a.index('a'))
+#print(a.isalpha())
+#print(a.endswith('t'))
+#b='Sharma' 'Sharma'
+#print(b.casefold())
+#print(b.join('r'))
+
+#a=[2,2,3,[4,5,6]]
+#print(a)
+#b=a[3]
+#print(b)
+#k=b[0]
+#print(k)
+
+a=[2,4,5,6]
+print(a)
